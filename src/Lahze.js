276311b73acd55ffa740db30e8a0e06460244b12
Pouya <pouya.mozafar@gmail.com>
2020-01-11 22:05:43 +0330

    refactor: add functions as utils

--- src/Lahze.js
+++ src/Lahze.js
@@ -1,5 +1,6 @@
-import toGregorian from './utils/toGregorian';
 import garantee2Digits from './utils/garantee2Digits';
+import transformFromFormat from './utils/transformFromFormat';
+import parse from './utils/parse';
 
 const DATE_FORMATS = {
   FULL_YEAR: /YYYY/,
@@ -21,7 +22,8 @@ export default function Lahze(time, format, locale) {
 
   if(time){
     if(format){
-      this._date = transformFromFormat(time, format, locale);
+      const { year, month, day } = transformFromFormat(time, format, locale);
+      this._date = new Date(year, month - 1, day);
     }else {
       if(time instanceof Date){
         this._date = time;
@@ -56,98 +58,4 @@ Lahze.prototype.format = function(format, locale) {
   format = format.replace(DATE_FORMATS.SHORT_SECONDS, this._date.getSeconds());
 
   return format;
-}
-
-function parse({ date, locale }){
-  const localeDate = date.toLocaleDateString(locale);
-  let output;
-  let parsed;
-
-  if(locale === 'fa'){
-    output = /(\d{4})\/(\d{1,2})\/(\d{1,2})/.exec(
-      convertNumber(localeDate)
-    );
-    parsed = {
-      Y: output[1],
-      M: output[2],
-      D: output[3],
-    }
-  }else {
-    output = /(\d{1,2})\/(\d{1,2})\/(\d{4})/.exec(
-      convertNumber(localeDate)
-    );
-    parsed = {
-      Y: output[3],
-      M: output[1],
-      D: output[2],
-    }
-  };
-
-  return parsed;
-}
-
-function convertNumber(input){
-  const persianNumbers = {
-    '۱': 1,
-    '۲': 2,
-    '۳': 3,
-    '۴': 4,
-    '۵': 5,
-    '۶': 6,
-    '۷': 7,
-    '۸': 8,
-    '۹': 9,
-    '۰': 0,
-  };
-
-  return input.split('').map(char => {
-    if(char in persianNumbers) return persianNumbers[char];
-    return char;
-  }).join('');
-}
-
-function transformFromFormat(time, format, locale){
-  const result = fromFromat(time, format, locale);
-
-  return new Date(result.year, result.month - 1, result.day);
-}
-
-function fromFromat(time, format, locale){
-  const year = {
-    string: [DATE_FORMATS.FULL_YEAR, DATE_FORMATS.SHORT_YEAR],
-    regexp: ['\\d{4}', '\\d{2}'],
-    property: 'year',
-  };
-  const month = {
-    string: [DATE_FORMATS.FULL_MONTH, DATE_FORMATS.SHORT_MONTH],
-    regexp: ['\\d{2}', '\\d{1,2}'],
-    property: 'month',
-  }
-  const day = {
-    string: [DATE_FORMATS.FULL_DAY, DATE_FORMATS.SHORT_DAY],
-    regexp: ['\\d{2}', '\\d{1,2}'],
-    property: 'day',
-  };
-
-  const dates = [year, month, day];
-
-  const result = {};
-
-  dates.forEach(({ string, regexp, property })=>{
-    for(let i = 0;i<string.length;i++){
-      if(string[i].test(format)){
-        const tempTime = format.replace(string[i], 'P').replace(/[^P\-/\\]/g, '.').replace('P', `(${regexp[i]})`);
-        result[property] = new RegExp(tempTime).exec(time)[1];
-      }
-    }
-  })
-
-  if(locale === 'fa') {
-    const [gYear, gMonth, gDay] = toGregorian(result.year, result.month, result.day);
-    result.year = gYear;
-    result.month = gMonth;
-    result.day = gDay;
-  }
-
-  return result;
 }
---
+++ src/utils/transformFromFormat/index.js
@@ -0,0 +1,47 @@
+import toGregorian from './toGregorian';
+
+export default function transformFromFormat(time, format, locale){
+  const result = fromFromat(time, format, locale);
+
+  return result;
+}
+
+function fromFromat(time, format, locale){
+  const year = {
+    string: [DATE_FORMATS.FULL_YEAR, DATE_FORMATS.SHORT_YEAR],
+    regexp: ['\\d{4}', '\\d{2}'],
+    property: 'year',
+  };
+  const month = {
+    string: [DATE_FORMATS.FULL_MONTH, DATE_FORMATS.SHORT_MONTH],
+    regexp: ['\\d{2}', '\\d{1,2}'],
+    property: 'month',
+  }
+  const day = {
+    string: [DATE_FORMATS.FULL_DAY, DATE_FORMATS.SHORT_DAY],
+    regexp: ['\\d{2}', '\\d{1,2}'],
+    property: 'day',
+  };
+
+  const dates = [year, month, day];
+
+  const result = {};
+
+  dates.forEach(({ string, regexp, property })=>{
+    for(let i = 0;i<string.length;i++){
+      if(string[i].test(format)){
+        const tempTime = format.replace(string[i], 'P').replace(/[^P\-/\\]/g, '.').replace('P', `(${regexp[i]})`);
+        result[property] = new RegExp(tempTime).exec(time)[1];
+      }
+    }
+  })
+
+  if(locale === 'fa') {
+    const [gYear, gMonth, gDay] = toGregorian(result.year, result.month, result.day);
+    result.year = gYear;
+    result.month = gMonth;
+    result.day = gDay;
+  }
+
+  return result;
+}
